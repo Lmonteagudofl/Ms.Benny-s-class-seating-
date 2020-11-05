dfb218ddace0e5a9c88c98c3ca5df27cf6771884
lmonteagudo15 <lyanna.monteagudo@patelhs.org>
2020-11-05 17:20:19 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[] args) {
+    int [] seatNumbers = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+     
+      String [] studentNames = new int[15];
+      
+      for (int i = 0; i < seatNumbers.length; i++)
+      System.out.println("What is the students name? ")
+
+      Scanner scan = new scanner(System.in);
+
+  }
+}
