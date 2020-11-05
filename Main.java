7ae6bc261eedaff60a8b05e585daa406ca82672a
lmonteagudo15 <lyanna.monteagudo@patelhs.org>
2020-11-05 17:51:51 +0000

    i changed all the variables to b to see if my code would run better

--- Main.java
+++ Main.java
@@ -1,16 +1,21 @@
+import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
     int[] seatNumbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+    String[] studentNames = new String[15];
 
-    String[] studentNames = new int[15];
     Scanner scan = new Scanner(System.in);
 
     for (int i = 0; i < seatNumbers.length; i++) {
       System.out.println("What is the student's name?");
       studentNames [i] = scan.next();
     }
-    int i = 0
-    while (i < seatNumbers.length);
+
+    int b = 0;
+    while (b < seatNumbers.length);
     {
+      System.out.println(studentNames[b] + "sits in seat # " + seatNumbers[b]);
+      b++;
+    }
   }
 }
