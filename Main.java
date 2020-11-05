17baffcd88192a703de767058ca2d4f9eedb396f
lmonteagudo15 <lyanna.monteagudo@patelhs.org>
2020-11-05 17:36:31 +0000

    i added a while loop

--- Main.java
+++ Main.java
@@ -1,13 +1,16 @@
 class Main {
   public static void main(String[] args) {
-    int [] seatNumbers = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
-     
-      String []studentNames = new int[15];
-      
-      for (int i = 0; i < seatNumbers.length; i++)
+    int[] seatNumbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+
+    String[] studentNames = new int[15];
+    Scanner scan = new Scanner(System.in);
+
+    for (int i = 0; i < seatNumbers.length; i++) {
       System.out.println("What is the student's name?");
-
-      Scanner scan = new scanner(System.in);
-
+      studentNames [i] = scan.next();
+    }
+    int i = 0
+    while (i < seatNumbers.length);
+    {
   }
 }
