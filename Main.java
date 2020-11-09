6213b7d0fef23b5198c80c1e8c3d6aa24ed708b4
lmonteagudo15 <lyanna.monteagudo@patelhs.org>
2020-11-09 15:03:16 +0000

    i added statements

--- Main.java
+++ Main.java
@@ -9,10 +9,11 @@ class Main {
     for (int i = 0; i < seatNumbers.length; i++) {
       System.out.println("What is the student's name?");
       studentNames [i] = scan.next();
+      // ^^ gets input from the user for 15 names// 
     }
-
     int b = 0;
     while (b < seatNumbers.length);
+    // while statement if b is less than the seat numbers it prints out the statement// 
     {
       System.out.println(studentNames[b] + "sits in seat # " + seatNumbers[b]);
       b++;
